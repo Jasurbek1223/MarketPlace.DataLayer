da60a2037edb2df897bee36e80811b0c02828e7b
Jasurbek1223 <jasurabdulxaev@gmail.com>
2023-07-31 18:12:49 +0500

    sab

---
+++ MarketPlace.Console/Program.cs
@@ -0,0 +1,9 @@
+﻿namespace MarketPlace.Console;
+
+internal class Program
+{
+    static void Main(string[] args)
+    {
+        
+    }
+}
---
+++ MarketPlace.ServiceLayer/Class1.cs
@@ -0,0 +1,7 @@
+﻿namespace MarketPlace.ServiceLayer
+{
+    public class Class1
+    {
+
+    }
+}
